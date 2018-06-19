33f6c478b5ab578251ed52f1e61aa6a2b7c917d0
John A. Fedoruk <johnny@johnfedoruk.ca>
2018-06-18 20:10:29 -0500

    Added tests for Lottery's enter method

--- test/lottery.spec.js
+++ test/lottery.spec.js
@@ -13,16 +13,78 @@ const ARGS = [];
 describe('Lottery', () => {
     beforeEach(async () => {
         // Get a list of all accounts
-        fetchedAccounts = await web3.eth.getAccounts();
+        accounts = await web3.eth.getAccounts();
         // use an account to deploy the contract
         lottery = await new web3.eth.Contract(JSON.parse(interface))
             .deploy({ data: `0x${bytecode}`, arguments: ARGS })
-            .send({ from: fetchedAccounts[0], gas: MAX_GAS });
+            .send({ from: accounts[0], gas: MAX_GAS });
     });
     it('should fetch accounts', () => {
-        assert.ok(fetchedAccounts);
+        expect(accounts).not.to.be.undefined;
     })
     it('deploys a contract', () => {
-        assert.ok(lottery.options.address);
+        expect(lottery.options.address).not.to.be.undefined;
+    });
+    describe('enter', () => {
+        it('should fail if less then 0.01 ether is added', async () => {
+            const wager = 0.01 - 0.0000001;
+            try {
+                await lottery.methods.enter().send({
+                    from: accounts[0],
+                    value: web3.utils.toWei(`${wager}`, 'ether')
+                });
+                chai.assert.fail();
+            } catch (e) {
+
+            }
+            // const players = await lottery.methods.viewPlayers().call({
+            //     from: accounts[0]
+            // });
+        });
+        it('should fail if more then 0.01 ether is added', async () => {
+            const wager = 0.01 + 0.0000001;
+            try {
+                await lottery.methods.enter().send({
+                    from: accounts[0],
+                    value: web3.utils.toWei(`${wager}`, 'ether')
+                });
+                chai.assert.fail();
+            } catch (e) {
+
+            }
+        });
+        it('should add user if exactly 0.01 ether is added', async () => {
+            const wager = 0.01;
+            await lottery.methods.enter().send({
+                from: accounts[0],
+                value: web3.utils.toWei(`${wager}`, 'ether')
+            });
+            const players = await lottery.methods.viewPlayers().call({
+                from: accounts[0]
+            });
+            expect(players[0]).to.equal(accounts[0]);
+        });
+        it('should allow multiple users to enter', async () => {
+            const wager = 0.01;
+            await (async()=>{
+                return new Promise(
+                    (resolve) => {
+                        accounts.forEach(async (account, index) => {
+                            await lottery.methods.enter().send({
+                                from: account,
+                                value: web3.utils.toWei(`${wager}`, 'ether')
+                            });
+                            if(index===accounts.length-1) {
+                                resolve();
+                            }
+                        });
+                    }
+                );
+            })();
+            const players = await lottery.methods.viewPlayers().call({
+                from: accounts[0]
+            });
+            expect(players).deep.to.equal(accounts);
+        });
     });
 });
